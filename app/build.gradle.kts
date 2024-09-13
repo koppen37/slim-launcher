eb9bf65af31f8e90893af6637bf2ce0136ca3650
Vincent Koppen <vincent@koppen.email>
2024-09-13 15:24:30 +0200

    chore: update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,11 +107,11 @@ dependencies {
     // Integration with activities
     implementation("androidx.activity:activity-compose:1.9.2")
     // Compose Material Design
-    implementation("androidx.compose.material:material:1.7.0")
+    implementation("androidx.compose.material:material:1.7.1")
     // Animations
-    implementation("androidx.compose.animation:animation:1.7.0")
+    implementation("androidx.compose.animation:animation:1.7.1")
     // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.7.0")
+    implementation("androidx.compose.ui:ui-tooling:1.7.1")
     // Integration with ViewModels
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
     // When using a AppCompat theme
